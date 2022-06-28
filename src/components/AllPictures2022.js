05d5d2ef00de62cfde4d4843c25a2481795d8db4
Mariana Lozynska <mlozynska79@gmail.com>
2022-06-28 09:54:16 +0100

    new months

--- src/components/AllPictures2022.js
+++ src/components/AllPictures2022.js
@@ -90,8 +90,8 @@ const AllPictures2022 = () => {
             {/* <option value="9">September</option> */}
             {/* <option value="8">August</option> */}
             {/* <option value="7">July</option> */}
-            {/* <option value="6">June</option> */}
-            {/* <option value="5">May</option> */}
+            <option value="6">June</option>
+            <option value="5">May</option>
             <option value="4">April</option>
             <option value="3">March</option>
             <option value="2">February</option>
